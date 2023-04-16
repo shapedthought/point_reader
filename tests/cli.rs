8df0396604d5bfb93c438e77349d9b59b00768db
Edward Howard <edward.x.howard@veeam.com>
2023-04-16 07:49:34 +0100

    Tests Added

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,7 +1,7 @@
 use assert_cmd::Command;
 
 #[test]
-fn runs() {
+fn run_all() {
     let mut cmd = Command::cargo_bin("point_reader").unwrap();
 
     cmd.args(&[
@@ -9,6 +9,102 @@ fn runs() {
         "tests\\VSE_test.json",
         "-w",
         "Test",
+    ]);
+
+    cmd.assert().success();
+}
+
+#[test]
+fn perf_only_test() {
+    let mut cmd = Command::cargo_bin("point_reader").unwrap();
+
+    cmd.args(&[
+        "-f",
+        "tests\\VSE_test.json",
+        "-w",
+        "Test",
+        "-t",
+        "p",
+    ]);
+
+    cmd.assert().success();
+}
+
+#[test]
+fn cap_only_test() {
+    let mut cmd = Command::cargo_bin("point_reader").unwrap();
+
+    cmd.args(&[
+        "-f",
+        "tests\\VSE_test.json",
+        "-w",
+        "Test",
+        "-t",
+        "c",
+    ]);
+
+    cmd.assert().success();
+}
+
+#[test]
+fn arch_only_test() {
+    let mut cmd = Command::cargo_bin("point_reader").unwrap();
+
+    cmd.args(&[
+        "-f",
+        "tests\\VSE_test.json",
+        "-w",
+        "Test",
+        "-t",
+        "a",
+    ]);
+
+    cmd.assert().success();
+}
+
+#[test]
+fn perf_cap_test() {
+    let mut cmd = Command::cargo_bin("point_reader").unwrap();
+
+    cmd.args(&[
+        "-f",
+        "tests\\VSE_test.json",
+        "-w",
+        "Test",
+        "-t",
+        "pc",
+    ]);
+
+    cmd.assert().success();
+}
+
+#[test]
+fn perf_arch_test() {
+    let mut cmd = Command::cargo_bin("point_reader").unwrap();
+
+    cmd.args(&[
+        "-f",
+        "tests\\VSE_test.json",
+        "-w",
+        "Test",
+        "-t",
+        "pa",
+    ]);
+
+    cmd.assert().success();
+}
+
+#[test]
+fn cap_arch_test() {
+    let mut cmd = Command::cargo_bin("point_reader").unwrap();
+
+    cmd.args(&[
+        "-f",
+        "tests\\VSE_test.json",
+        "-w",
+        "Test",
+        "-t",
+        "ca",
     ]);
 
     cmd.assert().success();
